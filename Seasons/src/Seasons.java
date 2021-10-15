092d01d3e10bca6189c4a0828e2b5c4f6272c766
dmytro.novitskyi <dm.novitskiy+1@gmail.com>
2021-10-15 14:51:10 +0300

    made inputs case insensitive

--- Seasons/src/Seasons.java
+++ Seasons/src/Seasons.java
@@ -1,3 +1,4 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 
@@ -11,30 +12,30 @@ public class Seasons {
     main(String[] args) {
         System.out.println("Enter your month for us to tell you which season it belongs to thanks:");
         Scanner scanner = new Scanner(System.in);
-        String Month = scanner.nextLine();
+        String Month = scanner.nextLine().toLowerCase(Locale.ROOT);
         Season season;
         switch (Month) {
-            case "December":
-            case "January":
-            case "February":
+            case "december":
+            case "january":
+            case "february":
                 season = Season.WINTER;
                 System.out.println(season);
                 break;
-            case "March":
-            case "April":
-            case "May":
+            case "march":
+            case "april":
+            case "may":
                 season = Season.SPRING;
                 System.out.println(season);
                 break;
-            case "June":
-            case "July":
-            case "August":
+            case "june":
+            case "july":
+            case "august":
                 season = Season.SUMMER;
                 System.out.println(season);
                 break;
-            case "September":
-            case "October":
-            case "November":
+            case "september":
+            case "october":
+            case "november":
                 season = Season.AUTUMN;
                 System.out.println(season);
                 break;
